f79dba7000683c18658b7766a745e96f907743be
Leandro Lonardoni <leandro.lonardoni@benner.com.br>
2019-04-20 13:07:33 -0300

    .

--- server/etc/start_jbpm-wb.sh
+++ server/etc/start_jbpm-wb.sh
@@ -7,5 +7,5 @@ echo "Update database connection setup"
 export KIE_MAVEN_REPO="http://52.173.146.231:8080/business-central/maven2"
 
 echo "Running jBPM Server Full on JBoss Wildfly..."
-exec ./standalone.sh -b $JBOSS_BIND_ADDRESS $EXTRA_OPTS -Dorg.kie.server.location=$KIE_SERVER_LOCATION -Dorg.kie.server.id=$KIE_SERVER_ID -Dorg.kie.server.user=$KIE_SERVER_USER -Dorg.kie.server.pwd=$KIE_SERVER_PWD -Dorg.kie.server.controller=$KIE_SERVER_CONTROLLER -Dorg.kie.server.controller.user=$KIE_SERVER_CONTROLLER_USER -Dorg.kie.server.controller.pwd=$KIE_SERVER_CONTROLLER_PWD
+exec ./standalone.sh -b $JBOSS_BIND_ADDRESS $EXTRA_OPTS -Dorg.kie.server.location=$KIE_SERVER_LOCATION -Dorg.kie.server.id=$KIE_SERVER_ID -Dorg.kie.server.user=$KIE_SERVER_USER -Dorg.kie.server.pwd=$KIE_SERVER_PWD -Dorg.kie.server.controller=$KIE_SERVER_CONTROLLER -Dorg.kie.server.controller.user=$KIE_SERVER_CONTROLLER_USER -Dorg.kie.server.controller.pwd=$KIE_SERVER_CONTROLLER_PWD -c standalone-kie-server-full.xml
 exit $?
